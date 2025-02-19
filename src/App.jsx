0100a52b6237896f16c1087bb389ef24bf8524b4
abrorb3k <abrorb3k0219@gmail.com>
2025-02-19 20:19:32 +0500

    jhbhj

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,7 @@
-// import MainLayout from "./layouts/MainLayout";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Products from "../src/assets/Products"
-// import Details from "./pages/Details";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import { Routes, Route } from "react-router-dom";
-
-
-// import "./App.css";
-// import { createContext, useState } from "react";
-
-// export const CartContext = createContext();
-
-// function App() {
-
-//   const [cart, setCart] = useState([])
-
-//   return (
-//     <CartContext.Provider value={{cart, setCart}}>
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <MainLayout>
-//               <Home></Home>
-//             </MainLayout>
-//           }
-//         ></Route>
-//         <Route
-//           path="/about"
-//           element={
-//             <MainLayout>
-//               <About></About>
-//             </MainLayout>
-//           }
-//         ></Route>
-//         <Route
-//           path="/products"
-//           element={
-//             <MainLayout>
-//               <Products></Products>
-//             </MainLayout>
-//           }
-//         ></Route>
-//         <Route
-//           path="/products/:id"
-//           element={
-//             <MainLayout>
-//               <Details></Details>
-//             </MainLayout>
-//           }
-//         ></Route>
-//         <Route
-//           path="/cart"
-//           element={
-//             <MainLayout>
-//               <Cart></Cart>
-//             </MainLayout>
-//           }
-//         ></Route>
-//         <Route path="/login" element={<Login></Login>}></Route>
-//         <Route path="/register" element={<Register></Register>}></Route>
-//       </Routes>
-//     </CartContext.Provider>
-//   );
-// }
-
-// export default App;
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Products from "./pages/Products"; // To'g'rilangan import
+import Products from "./pages/Products";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
@@ -83,18 +11,13 @@ import { createContext, useState } from "react";
 
 import "./App.css";
 
-// CartContext yaratish
 export const CartContext = createContext();
 
 function App() {
   const [cart, setCart] = useState([]);
-  const [user, setUser] = useState(null); // Foydalanuvchi holatini kuzatish
 
-  // Authentifikatsiyani tekshirish (simple version)
   const ProtectedRoute = ({ children }) => {
-    if (!user) {
-      return <Login />; // Agar foydalanuvchi tizimga kirgan bo'lmasa, login sahifasiga o'tkazish
-    }
+    
     return children;
   };
 
@@ -143,8 +66,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login setUser={setUser} />} />{" "}
-        {/* Login komponentasiga userni belgilash */}
+        <Route path="/login" element={<Login/>} />{" "}
         <Route path="/register" element={<Register />} />
       </Routes>
     </CartContext.Provider>
