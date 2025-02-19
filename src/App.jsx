9514f059b80ad43fe245ebff191e1bec96fdce23
abrorb3k <abrorb3k0219@gmail.com>
2025-02-19 19:58:36 +0500

    jdsbc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Products from "/pages/products";
-import Products from "./pages/products";
-// import Products from "./pages/Product"
+import Products from "../src/pages/Products";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
